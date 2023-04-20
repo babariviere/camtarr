8c3c464fd38b69bbcf451531a5fd1c17312fc599
Bastien Riviere <me@babariviere.com>
2023-04-20 23:40:40 +0200

    feat: add config parsing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/babariviere/camtarr
 
 go 1.20
+
+require github.com/caarlos0/env/v8 v8.0.0
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/caarlos0/env/v8"
+)
+
+type tautulliConfig struct {
+	ApiKey       string `env:"API_KEY"`
+	Uri          string `env:"URI"`
+	ExporterPort string `env:"EXPORTER_PORT" envDefault:"9301"`
+}
+
+type config struct {
+	Tautulli tautulliConfig `envPrefix:"TAUTULLI_"`
+}
 
 func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalln("failed to parse env:", err)
+	}
+
+	fmt.Printf("Parsed env: %+v\n", cfg)
+
 	fmt.Println("Hello Marcel")
 }
